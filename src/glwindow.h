33d3fd636c005d67fe5578760a161519c8800662
Daniel <dangordon.za@gmail.com>
2024-04-20 14:59:06 +0200

    first commit

---
+++ src/glwindow.h
@@ -0,0 +1,27 @@
+#ifndef GL_WINDOW_H
+#define GL_WINDOW_H
+
+#include <GL/glew.h>
+
+#include "geometry.h"
+
+class OpenGLWindow
+{
+public:
+    OpenGLWindow();
+
+    void initGL();
+    void render();
+    bool handleEvent(SDL_Event e);
+    void cleanup();
+
+private:
+    SDL_Window* sdlWin;
+
+    GeometryData sun_geom;
+    GLuint vao;
+    GLuint shader;
+    GLuint vertexBuffer;
+};
+
+#endif
